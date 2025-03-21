fa750cf959ea99a61abe31dbb156198e77b585aa
Edige-prog <akimali.edige@gmail.com>
2025-03-21 15:10:39 +0500

    main

--- main.py
+++ main.py
@@ -12,6 +12,3 @@ app = FastAPI()
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
 app.include_router(flowers_router, prefix="/flowers", tags=["flowers"])
 app.include_router(cart_router, prefix="/cart", tags=["cart"])
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
