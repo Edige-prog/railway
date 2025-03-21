17c918df4503f861ca1833c81f9931ae5d5d6ccb
Edige-prog <akimali.edige@gmail.com>
2025-03-21 15:30:00 +0500

    init_tables

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ from pathlib import Path
 from api.auth import router as auth_router
 from api.flowers import router as flowers_router
 from api.cart import router as cart_router
+from database.db import init_tables
 import uvicorn
 
 app = FastAPI()
+
+@app.on_event("startup")
+async def startup_event():
+    init_tables()
 
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
 app.include_router(flowers_router, prefix="/flowers", tags=["flowers"])
